b274a64b55762be3328aef23bb9ac09f253626f5
Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>
2026-05-23 18:39:39 +0300

    chore: install doctrine/doctrine-fixtures-bundle package
    Signed-off-by: Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>

--- rest-api/config/bundles.php
+++ rest-api/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\AI\AiBundle\AiBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
