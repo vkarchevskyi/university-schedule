c067b2ee8b925d3d2b49784d5c6fa335f086d3ac
Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>
2026-05-24 18:50:23 +0300

    Add generation job list endpoints

--- rest-api/src/Controller/Admin/ExamScheduleGenerationJobController.php
+++ rest-api/src/Controller/Admin/ExamScheduleGenerationJobController.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Service\ExamScheduleGeneration\GetExamScheduleGenerationJobService;
+use App\Service\ExamScheduleGeneration\ListExamScheduleGenerationJobsService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/admin/exam-schedule-generation-jobs')]
 final class ExamScheduleGenerationJobController extends AbstractAdminController
 {
+    #[Route('', methods: ['GET'])]
+    public function list(ListExamScheduleGenerationJobsService $jobs): JsonResponse
+    {
+        return $this->respond(fn() => $jobs->list());
+    }
+
     #[Route('/{jobId}', methods: ['GET'])]
     public function get(string $jobId, GetExamScheduleGenerationJobService $jobs): JsonResponse
     {
--- rest-api/src/Controller/Admin/ScheduleGenerationJobController.php
+++ rest-api/src/Controller/Admin/ScheduleGenerationJobController.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Service\ScheduleGeneration\GetScheduleGenerationJobService;
+use App\Service\ScheduleGeneration\ListScheduleGenerationJobsService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/admin/generation-jobs')]
 final class ScheduleGenerationJobController extends AbstractAdminController
 {
+    #[Route('', methods: ['GET'])]
+    public function list(ListScheduleGenerationJobsService $jobs): JsonResponse
+    {
+        return $this->respond(fn() => $jobs->list());
+    }
+
     #[Route('/{jobId}', methods: ['GET'])]
     public function get(string $jobId, GetScheduleGenerationJobService $jobs): JsonResponse
     {
